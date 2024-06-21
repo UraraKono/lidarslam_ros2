c4cde9d1d788e8b1ba1935c9eddc6b0e9a175bf1
UraraKono <urara27.k@gmail.com>
2024-06-21 13:31:20 -0400

    F1fifth - add map, new topics and imu

--- lidarslam/launch/lidarslam.launch.py
+++ lidarslam/launch/lidarslam.launch.py
@@ -25,14 +25,16 @@ def generate_launch_description():
         package='scanmatcher',
         executable='scanmatcher_node',
         parameters=[main_param_dir],
-        remappings=[('/input_cloud','/velodyne_points')],
+        # remappings=[('/input_cloud','/points')],
+        remappings=[('/input_cloud','/ouster/points')],
         output='screen'
         )
 
     tf = launch_ros.actions.Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments=['0','0','0','0','0','0','1','base_link','velodyne']
+        # arguments=['0','0','0','0','0','0','1','base_link','laser_sensor_frame']
+        arguments=['0','0','0','0','0','0','1','base_link','os_sensor']
         )
 
 
